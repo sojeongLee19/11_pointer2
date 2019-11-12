1fc3a7fd8187685dcfd95d8423bba89653a2cc55
sojeongLee19 <55444000+sojeongLee19@users.noreply.github.com>
2019-11-12 15:44:44 +0900

    3

--- main.c
+++ main.c
@@ -5,16 +5,15 @@
 
 int main(void)
 {
-char *pc;
-int *pi;
-double *pd;
-pc = (char *)10000;
-pi = (int*)10000;
-pd = (double *)10000;
-printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
-pc++;
-pi++;
-pd++;
-printf("증가 후 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+    char buffer[8];
+    double *pd;
+    int *pi;
+    pd = (double *)buffer;
+    *pd = 3.14;
+    printf("%f\n", *(double*)buffer );
+    pi = (int *)buffer;
+    *pi = 123;
+    *(pi+1) = 456;
+    printf("%d %d\n", *(int*)buffer, *((int*)buffer + 1) );
 return 0;
 }
